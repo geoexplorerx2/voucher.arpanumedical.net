4a04809c25229afa3866f3a1c133efe0cd3c9056
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-28 08:01:46 -0700

    updated

--- resources/views/admin/PDFRepository/dataGenerator.blade.php
+++ resources/views/admin/PDFRepository/dataGenerator.blade.php
@@ -17,7 +17,8 @@
                             <option value="Mrs">Mrs</option>
                         </select>
                     </div>
-                    <div class="col-lg-7"><input id="fullname" type="text" class="form-control" style="transform: translateX(-10px)"></div>
+                    <div class="col-lg-7"><input id="fullname" type="text" class="form-control"
+                            style="transform: translateX(-10px)"></div>
 
                 </div>
             </div>
@@ -27,7 +28,7 @@
                 <label class="form-label">Country</label>
                 <select class="form-control select" id="countries">
                     @foreach($countries as $country)
-                    <option value="{{ $country->name }}">{{ $country->name }}</option>
+                        <option value="{{ $country->name }}">{{ $country->name }}</option>
                     @endforeach
                 </select>
             </div>
@@ -48,7 +49,8 @@
             <div class="mb-3">
                 <label class="form-label">surcharge for payment by credit card</label>
                 <div class="row">
-                    <div class="col-lg-8"><input id="surchargepayment" type="number" class="form-control" style="transform: translateX(-10px)"></div>
+                    <div class="col-lg-8"><input id="surchargepayment" type="number" class="form-control"
+                            style="transform: translateX(-10px)"></div>
                     <div class="col-lg-4">
                         <select class="form-control select" id="surchargepaymentValue">
                             <option value="€" selected> € </option>
@@ -64,7 +66,8 @@
             <div class="mb-3">
                 <label class="form-label">İn case DHI technique will be applied</label>
                 <div class="row">
-                    <div class="col-lg-8"><input id="DHI" type="number" class="form-control" style="transform: translateX(-10px)"></div>
+                    <div class="col-lg-8"><input id="DHI" type="number" class="form-control"
+                            style="transform: translateX(-10px)"></div>
                     <div class="col-lg-4">
                         <select class="form-control select" id="DHIValue">
                             <option value="€" selected> € </option>
@@ -73,6 +76,26 @@
                             <option value="£"> £ </option>
                         </select>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="mb-3">
+                <label class="form-label">Service</label>
+                <div class="row">
+                    <div class="mb-2">Hair Transplant Package</div>
+                    <ul style="list-style: none;">
+                        <li style="padding: 5px 0;"><span><input type="checkbox" value="Operation" /></span><span
+                                style="padding: 0 10px;">Operation</span></li>
+                        <li style="padding: 5px 0;"><span><input type="checkbox" value="Airport Transfers" /></span><span
+                                style="padding: 0 10px;">Airport Transfers</span></li>
+                        <li style="padding: 5px 0;"><span><input type="checkbox" value="Hotel" /></span><span
+                                style="padding: 0 10px;">Hotel</span></li>
+                        <li style="padding: 5px 0;"><span><input type="checkbox" value="Flights" /></span><span
+                                style="padding: 0 10px;">Flights</span></li>
+                        <li style="padding: 5px 0;"><span><input type="checkbox" value="Post-Op" /></span><span
+                                style="padding: 0 10px;">Post-Op</span></li>
+                        <ul>
                 </div>
             </div>
         </div>
